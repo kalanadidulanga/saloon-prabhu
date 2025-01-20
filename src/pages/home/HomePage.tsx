5632086bc1180ab655245563dd1cf15df55163fd
Kalana Didulanga Koralegedara <dev.kalanadidulanga@gmail.com>
2025-01-20 14:09:36 +0530

    fixed

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -9,26 +9,26 @@ import AboutSection3 from "./AboutSection3";
 import CustomersSection from "./CustomersSection";
 
 const HomePage = () => {
-  const SERVICES = [
-    {
-      imageUrl: "/assets/service1.png",
-      title: "Professional Haircuts and Styling",
-      description:
-        "From timeless, classic cuts to trendy, modern styles, we cater to both men and women. Whether you're looking for a sharp professional look, a casual everyday style, or a bold new transformation, our expert stylists tailor each cut to suit your personality and preferences. We ensure you leave with a style that enhances your unique features and confidence.",
-    },
-    {
-      imageUrl: "/assets/service2.png",
-      title: "Bridal and Event Makeup",
-      description:
-        "Create unforgettable memories with our glamorous makeup and hairstyling services for weddings, parties, and special occasions. From natural, elegant looks to bold, head-turning styles, we customize each look to suit your outfit, theme, and personality. Our expert team ensures long-lasting, flawless results that keep you radiant throughout your special day.",
-    },
-    {
-      imageUrl: "/assets/service3.png",
-      title: "Hair Treatments and Coloring",
-      description:
-        "Revitalize your hair with our luxurious hair spa treatments, designed to nourish and strengthen your locks. Our keratin treatments restore smoothness and shine, leaving your hair frizz-free and manageable. Explore our customized hair color services, from subtle highlights to bold transformations, all tailored to enhance your unique style and personality.",
-    },
-  ];
+  // const SERVICES = [
+  //   {
+  //     imageUrl: "/assets/service1.png",
+  //     title: "Professional Haircuts and Styling",
+  //     description:
+  //       "From timeless, classic cuts to trendy, modern styles, we cater to both men and women. Whether you're looking for a sharp professional look, a casual everyday style, or a bold new transformation, our expert stylists tailor each cut to suit your personality and preferences. We ensure you leave with a style that enhances your unique features and confidence.",
+  //   },
+  //   {
+  //     imageUrl: "/assets/service2.png",
+  //     title: "Bridal and Event Makeup",
+  //     description:
+  //       "Create unforgettable memories with our glamorous makeup and hairstyling services for weddings, parties, and special occasions. From natural, elegant looks to bold, head-turning styles, we customize each look to suit your outfit, theme, and personality. Our expert team ensures long-lasting, flawless results that keep you radiant throughout your special day.",
+  //   },
+  //   {
+  //     imageUrl: "/assets/service3.png",
+  //     title: "Hair Treatments and Coloring",
+  //     description:
+  //       "Revitalize your hair with our luxurious hair spa treatments, designed to nourish and strengthen your locks. Our keratin treatments restore smoothness and shine, leaving your hair frizz-free and manageable. Explore our customized hair color services, from subtle highlights to bold transformations, all tailored to enhance your unique style and personality.",
+  //   },
+  // ];
 
   const CUSTOMERS = [
     {
