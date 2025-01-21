2bf0a6845a34ef04b5f25b3ae357ebc8ad7b0261
Kalana Didulanga Koralegedara <dev.kalanadidulanga@gmail.com>
2025-01-21 11:00:12 +0530

    gqqwwe

--- src/pages/services/ServicesPage.tsx
+++ src/pages/services/ServicesPage.tsx
@@ -1,5 +1,30 @@
+import { ChevronRight } from "lucide-react";
+import { Link } from "react-router";
+import ServicesSection from "./ServicesSection";
+
 const ServicesPage = () => {
-  return <div>ServicesPage</div>;
+  return (
+    <>
+      <div className="bg-[url('/assets/pageheaderbg.svg')] bg-cover bg-center">
+        <div className="container flex flex-col py-24 items-center">
+          <h1 className=" text-5xl md:text-6xl text-primary-bg font-judson">
+            Our Services
+          </h1>
+          <div className=" flex items-center text-primary-bg mt-4 gap-1 text-sm">
+            <Link
+              to={"../"}
+              className=" capitalize font-normal hover:underline"
+            >
+              Home
+            </Link>
+            <ChevronRight size={16} /> Services
+          </div>
+        </div>
+      </div>
+
+      <ServicesSection />
+    </>
+  );
 };
 
 export default ServicesPage;
