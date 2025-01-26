ee85666e43c0fb8994e437493aa8f2fd0a720436
Kalana Didulanga Koralegedara <dev.kalanadidulanga@gmail.com>
2025-01-26 17:13:58 +0530

    upgrade

--- src/pages/home/HeroSection2.tsx
+++ src/pages/home/HeroSection2.tsx
@@ -1,17 +1,46 @@
+import { useState, useEffect } from "react";
 import { Button } from "@/components/ui/button";
 import { Link } from "react-router";
 
 const HeroSection2 = () => {
+  // Array of background images for the slider
+  const backgroundImages = [
+    "/assets/herobg.svg",
+    "/assets/herobg.svg",
+    "/assets/herobg.svg", // Add more background images here
+  ];
+
+  const [currentBgIndex, setCurrentBgIndex] = useState(0);
+
+  useEffect(() => {
+    // Auto-play slider logic
+    const slideInterval = setInterval(() => {
+      setCurrentBgIndex(
+        (prevIndex) => (prevIndex + 1) % backgroundImages.length
+      );
+    }, 5000); // Change background every 5 seconds
+
+    // Cleanup interval on component unmount
+    return () => clearInterval(slideInterval);
+  }, []);
+
   return (
-    <div className="bg-[url('/assets/herobg.svg')] bg-cover bg-center">
+    <div
+      className="bg-cover bg-center transition-all duration-1000 ease-in-out"
+      style={{
+        backgroundImage: `url(${backgroundImages[currentBgIndex]})`,
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+      }}
+    >
       <div className="container flex flex-col min-h-[calc(100vh-96px)] justify-center pt-32 pb-24">
         <div className="w-full flex flex-col lg:flex-row">
-          <div className=" flex flex-col w-full motion-preset-slide-right ">
+          <div className="flex flex-col w-full motion-preset-slide-right">
             <h1 className="text-5xl md:text-6xl font-judson text-white">
-              Get the hair you’ve been <br />
+              Get the hair you've been <br />
               dreaming about
             </h1>
-            <p className=" text-white mt-6">
+            <p className="text-white mt-6">
               Hair and beauty studio,{" "}
               <span className="font-bold">Unisex saloon</span>
             </p>
@@ -21,15 +50,26 @@ const HeroSection2 = () => {
               className="mt-16 mr-auto tracking-wider"
               asChild
             >
-              <Link to={"../about"} className=" capitalize font-normal">
+              <Link to={"../about"} className="capitalize font-normal">
                 LEARN MORE
               </Link>
             </Button>
           </div>
-          {/* <div className=" w-full relative flex justify-end mt-8 lg:mt-0">
-            <img src="/assets/hero2.png" className=" w-full max-w-md" />
-          </div> */}
         </div>
+      </div>
+
+      {/* Optional: Slider Dots Indicator */}
+      <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 flex space-x-2">
+        {backgroundImages.map((_, index) => (
+          <button
+            key={index}
+            onClick={() => setCurrentBgIndex(index)}
+            className={`
+              w-3 h-3 rounded-full 
+              ${index === currentBgIndex ? "bg-white" : "bg-white/50"}
+            `}
+          />
+        ))}
       </div>
     </div>
   );
