8025a6fa23107423dd39db27391a49396e7f240c
Kalana Didulanga Koralegedara <dev.kalanadidulanga@gmail.com>
2025-01-28 23:39:27 +0530

    asdad

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,7 +11,6 @@ import {
   X,
 } from "lucide-react";
 import { NAVIGATIONS } from "@/constants";
-import { Link as ScrollLink } from "react-scroll";
 import useContactDetailsStore from "@/stores/contactdetailsStore";
 
 const Navbar = () => {
