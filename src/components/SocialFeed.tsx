c436d3f237d75a387eb81dc6919f04efda5180d1
Kalana Didulanga Koralegedara <kalanadidulanga2005@gmail.com>
2025-01-31 10:05:31 +0530

    asdasd

--- src/components/SocialFeed.tsx
+++ src/components/SocialFeed.tsx
@@ -45,7 +45,7 @@ const SocialFeed = () => {
         </div>
       ) : (
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-          {posts.map((post, index) => (
+          {posts.map((post) => (
             <a
               key={post.shortcode}
               href={`https://www.instagram.com/p/${post.shortcode}`}
