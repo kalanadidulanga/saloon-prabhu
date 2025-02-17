c8aafae526680ec9211769c7b4f6cd38055929e8
Kalana Didulanga Koralegedara <kalanadidulanga2005@gmail.com>
2025-02-17 11:13:37 +0530

    dfgd

--- src/pages/home/ReviewSection.tsx
+++ src/pages/home/ReviewSection.tsx
@@ -126,6 +126,11 @@ const ReviewSection = () => {
           </h2>
 
           <div className="bg-reviewbg bg-cover bg-no-repeat w-full relative ps-16 pe-14 py-12 md:px-20 lg:ps-28 lg:pe-24 lg:py-20 transition-all duration-300">
+            <img
+              src="/assets/R.jpg"
+              alt="Review Background"
+              className="absolute top-0 left-0 w-full h-full transition-all duration-100"
+            />
             {/* Slider container */}
             <div className="w-full h-full relative">
               {loading ? (
